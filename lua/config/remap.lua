5f5d397f9d506fdf1ebb1e1a8535860eac8f24c9
Tom <tom@ip-192-168-11-172.eu-west-2.compute.internal>
2025-02-20 11:20:19 +0000

    quick fix fix

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -31,9 +31,22 @@ vim.keymap.set("n", "<leader>l", ":wincmd l<CR>")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
+vim.keymap.set("v", "<", "<gv", { noremap = true, silent = true, desc = "Keep selection after indenting left" })
+vim.keymap.set("v", ">", ">gv", { noremap = true, silent = true, desc = "Keep selection after indenting right" })
+
 vim.keymap.set(
     "n",
     "<leader>ee",
     "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
 )
 
+vim.keymap.set("n", "<leader>q", function()
+  local windows = vim.fn.getwininfo()
+  for _, win in pairs(windows) do
+    if win.quickfix == 1 then
+      vim.cmd("cclose")
+      return
+    end
+  end
+  vim.cmd("copen")
+end, { noremap = true, silent = true, desc = "Toggle Quickfix Window" })
